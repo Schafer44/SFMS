a58515810b47688d08eaaf3de6f79b78ec5ed2ea
Schafer44 <tanner44schafer@gmail.com>
2022-08-17 22:15:58 -0500

    Bug fix

--- screens/all_Folder/allJSA.jsx
+++ screens/all_Folder/allJSA.jsx
@@ -80,7 +80,7 @@ export default function AllJSA(props) {
                           onPress={() =>
                             props.navigation.navigate("JSA", { file })
                           }
-                          title={"New Sheet"}
+                          title={"New JSA"}
                         ></Button>
                       </View>
                       {visible ? (
